79dbdd7e47ad6d7575d4f2a8e77795a4c10091e2
nguyenleviet <85124005+nguyenlevid@users.noreply.github.com>
2023-02-26 22:16:36 -0600

    Add frontend and  backend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi team Oriole
+      </header>
+    </div>
+  );
+}
+
+export default App;
