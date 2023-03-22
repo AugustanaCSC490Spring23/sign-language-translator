1a21e4b06754f62da18ae162baaa4142b2c9e2ed
nguyenleviet <85124005+nguyenlevid@users.noreply.github.com>
2023-03-22 14:40:32 -0500

    Implement handle unhandled exceptions: invalid secret keys...

--- server/server.js
+++ server/server.js
@@ -11,12 +11,19 @@ mongoose.set("strictQuery", false);
 console.log(`In mode: ${process.env.NODE_ENV}`);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected to MongoDB successfully");
 });
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+})
