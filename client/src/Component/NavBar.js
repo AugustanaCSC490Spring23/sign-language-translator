d1679fb04fde2ce9924316fd9f83a3fd288e5ded
Az_Ng <61089434+DrRechauffer@users.noreply.github.com>
2023-03-22 23:29:02 -0500

    Route done

---
+++ client/src/Component/NavBar.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "./NavBar.css";
+import { Link, Outlet } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <div>
+            <Navbar className='color-nav' expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link href="#learning">Learning</Nav.Link>
+                            <Nav.Link as={Link} to="/translator">Translator</Nav.Link>
+                            <Nav.Link href="#quiz">Quiz</Nav.Link>
+                            <Nav.Link href="#about-us">About Us</Nav.Link>
+                            <Nav.Link as={Link} to="/my-account">My Account</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet />
+        </div>
+    );
+}
+
+export default NavBar;
