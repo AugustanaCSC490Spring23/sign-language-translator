8e9bf25efca4afae72ffef61c1263ec7d7fd9ac8
Az_Ng <61089434+DrRechauffer@users.noreply.github.com>
2023-03-22 23:48:42 -0500

    fix spaghetti

--- client/src/pages/Home Page/Home.js
+++ client/src/pages/Home Page/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import "./Home.css";
 
 const Home = () => {
- 
+
     return (
-        <div className='backgroundcolor'>
-            <h1>SIGN LANGUAGE TRANSLATOR</h1>
-        </div>
-        )
+        <Container fluid>
+            <div className='backgroundcolor'>
+                <h1>SIGN LANGUAGE TRANSLATOR</h1>
+            </div>
+        </Container>
+    )
 }
 
 export default Home;
