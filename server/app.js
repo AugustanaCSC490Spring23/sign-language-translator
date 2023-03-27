2934a5c66dac42a61fbd0bfcfbde355fdc790374
nguyenleviet <85124005+nguyenlevid@users.noreply.github.com>
2023-03-27 10:58:38 -0500

    secutity: rate limit and headers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const sanitizer = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const Err = require("./utils/customError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,16 +15,32 @@ const personalItemRouter = require("./routes/personalItemRoutes");
 const app = express();
 
 //// GLOBAL MIDDLEWARES
+
+// security HTTP headers
+app.use(helmet());
+
+// allow json
+app.use(express.json({ limit: "10kb" }));
+
+// nosql query injection prevention !!DANGEROUS MALICIOUS HACKING METHOD
+// more info: https://www.imperva.com/learn/application-security/nosql-injection/
+app.use(sanitizer());
+
+// data sanitization
+app.use(xss());
+
+// prevent too many request from the same IP
 const limiter = rateLimit({
-  // prevent too many request from the same IP
   max: 100,
   windowMs: 3600000,
   message: "Too many requests. Try again in an hour.",
 });
 app.use("/api", limiter); // only apply limiter to api routes
-app.use(cors());
-app.use(express.json());
 
+// allow cors
+app.use(cors());
+
+// other middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
