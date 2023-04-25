3063698706d440d00d9bd9860a8f4966cbd55094
Az_Ng <61089434+DrRechauffer@users.noreply.github.com>
2023-04-25 09:30:08 -0500

    word details

--- client/src/pages/Lessons Page/Lessons.js
+++ client/src/pages/Lessons Page/Lessons.js
@@ -10,28 +10,28 @@ const Lessons = () => {
         <Container fluid className="lessons-container">
             <Row className="lessons-top-page">
                 <Col className="lessons-welcome" md={{ span: 3, offset: 4 }}>
-                    <h4>Hi Henry!</h4>
-                    <p>Welcome back<br></br>
-                        to my class
-                    </p>
+                    <div className="welcome-title">
+                        <h4>Hi Henry!</h4>
+                        <p>Welcome back<br></br>
+                            to my class
+                        </p>
+                    </div>
                 </Col>
                 <Col className="lessons-card-locate" md={{ span: 3 }}>
-                    <Card className="lessons-card">
-                        <Card.Body>
-                            <h4>Lesson 2</h4>
-                            <p>Hello and welcome!</p>
-                            <Row>
-                                <Col md={{ span: 8, offset: 2 }}>
-                                    <CusButton
-                                        bgcolor="#3B1404"
-                                        color="#F8E5DA"
-                                        radius="25"
-                                        title="Continue"
-                                        weight="750" />
-                                </Col>
-                            </Row>
-                        </Card.Body>
-                    </Card>
+                    <div className="lessons-card">
+                        <h4>Lesson 2</h4>
+                        <p>Hello and welcome!</p>
+                        <Row>
+                            <Col md={{ span: 8, offset: 2 }}>
+                                <CusButton
+                                    bgcolor="#3B1404"
+                                    color="#F8E5DA"
+                                    radius="25"
+                                    title="Continue"
+                                    weight="750" />
+                            </Col>
+                        </Row>
+                    </div>
                 </Col>
             </Row>
             <Row>
