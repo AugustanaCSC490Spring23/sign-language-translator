b14e70c5c0b316b38d76b461a84a34a69f6cc424
samuelteklemariam <90398694+samuelteklemariam@users.noreply.github.com>
2023-05-22 10:35:56 -0500

    flashCards2

--- client/src/services/flashcardsService.js
+++ client/src/services/flashcardsService.js
@@ -1,25 +1,30 @@
-// flashcardsService.js
+import axiosInstance from "../utils/axiosInstance";
+import errorHandler from "../utils/errorHandler";
 
-// Assume you have a collection of flashcards stored in an array or fetched from a data source
-const flashcardsCollection = [
-    {
-      id: 1,
-      question: 'What is the capital of France?',
-      answer: 'Paris'
-    },
-    {
-      id: 2,
-      question: 'What is the symbol for sodium?',
-      answer: 'Na'
-    },
-    // More flashcards...
-  ];
-  
-  // Function to retrieve a flashcards collection by its ID
-  export function getFlashcardsCollectionById(id) {
-    // Find the flashcard collection with the matching ID
-    const collection = flashcardsCollection.find(collection => collection.id === id);
-  
-    // Return the found collection if it exists, otherwise return null
-    return collection || null;
+
+const getFlashcardsCollectionById = async (slug) => {
+  try {
+    const response = await axiosInstance.get(
+      `/api/v1/flashcards/${slug}`,
+    );
+    return response;
+  } catch (error) {
+    errorHandler(error.message);
   }
+};
+
+
+const getAllFlashcardsCollections = async () => {
+  try {
+    const response = await axiosInstance.get(`/api/v1/flashcards`);
+    return response;
+  } catch (error) {
+    errorHandler(error.message);
+  }
+};
+
+
+export { getFlashcardsCollectionById, getAllFlashcardsCollections };
+
+
+
